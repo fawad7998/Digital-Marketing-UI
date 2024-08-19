abd013ed553d7e10e3ab622c8af15c64fcca45b5
Fawad Ahmad <fawad7998@gmail.com>
2024-08-19 16:23:32 +0500

    sadaddas

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,14 +1,11 @@
-"use client"
 import React from "react";
 import Navbar from "./components/Navbar";
-import TopNav from "./components/TopNav";
 import Home from "./Pages/Home/page";
 import DigitalMarketingCard from "./Pages/Details/page";
 
 const page = () => {
   return (
     <>
-      {/* <TopNav /> */}
       <Navbar />
       <Home />
       <div className="mt-20">
